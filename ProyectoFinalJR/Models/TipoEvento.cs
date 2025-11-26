fcddcfa3692d0aa2124e99b5fbb27b853d6c39ce
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-26 13:44:33 -0400

    Modificando el modelo

--- ProyectoFinalJR/Models/TipoEvento.cs
+++ ProyectoFinalJR/Models/TipoEvento.cs
@@ -2,20 +2,15 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoFinalJR.Models;
-
-[Table("tipo_evento")]
 public class TipoEvento
 {
     [Key]
-    [Display(Name = "ID")]
     public int TipoId { get; set; }
 
     [Required(ErrorMessage = "El nombre del tipo de evento es obligatorio.")]
     [StringLength(100, ErrorMessage = "El nombre no debe exceder los 100 caracteres.")]
-    [Display(Name = "Nombre del Tipo")]
     public string Nombre { get; set; } = string.Empty;
 
-    [Display(Name = "Descripción")]
     [StringLength(500)]
     public string? Descripcion { get; set; }
 
