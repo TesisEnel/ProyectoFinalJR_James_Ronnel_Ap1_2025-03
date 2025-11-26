f62a712669830487dfc8387c75c4f1ecaf326ffd
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-26 13:44:51 -0400

    Realizando el dbContexto

--- ProyectoFinalJR/Data/ApplicationDbContext.cs
+++ ProyectoFinalJR/Data/ApplicationDbContext.cs
@@ -1,9 +1,24 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoFinalJR.Models;
+using ProyectoFinalJR.Data;
 
 namespace ProyectoFinalJR.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TipoEvento> TiposEventos { get; set; } = default!;
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TipoEvento>();
+        }
     }
 }
