4f6f4b579e70b30c14620386369e50d9448ed798
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-26 23:06:46 -0400

    Realizando el modelo TipoProveedor

---
+++ ProyectoFinalJR/Models/TipoProveedor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalJR.Models;
+
+public class TipoProveedor
+{
+    [Key]
+    public int TipoProveedorId { get; set; }
+
+    [Required(ErrorMessage = "El nombre del tipo de proveedor es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no debe exceder los 100 caracteres.")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string? Descripcion { get; set; }
+}
