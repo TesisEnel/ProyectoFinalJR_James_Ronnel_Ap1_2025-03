bf66acd034630b740ce09d56b5517f0969fb8875
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-27 17:18:10 -0400

    Realizando el Appli

--- ProyectoFinalJR/Data/ApplicationUser.cs
+++ ProyectoFinalJR/Data/ApplicationUser.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
-namespace ProyectoFinalJR.Data
+namespace ProyectoFinalJR.Data;
+
+public class ApplicationUser : IdentityUser
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(150)]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El teléfono es obligatorio.")]
+    [StringLength(20)]
+    public string Telefono { get; set; } = string.Empty;
 
 }
