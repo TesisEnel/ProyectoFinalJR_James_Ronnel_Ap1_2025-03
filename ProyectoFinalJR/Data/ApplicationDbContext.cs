f7f19f8f4437a9fabddbca4c361b3c42386991a6
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-11-27 17:00:30 -0500

    Modificando el dbcontext

--- ProyectoFinalJR/Data/ApplicationDbContext.cs
+++ ProyectoFinalJR/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace ProyectoFinalJR.Data
         public DbSet<TipoEvento> TiposEventos { get; set; } = default!;
         public DbSet<TipoProveedor> TiposProveedores { get; set; } = default!; 
         public DbSet<Evento> Eventos { get; set; } = default!;
+        public DbSet<Cita> Citas { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -25,6 +26,8 @@ namespace ProyectoFinalJR.Data
             builder.Entity<Evento>()
             .Property(e => e.PresupuestoInicial)
              .HasPrecision(18, 2);
+
+            builder.Entity<Cita>();
         }
     }
 }
