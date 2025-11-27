b84790fae1c94f41b7c5acec02a6334c1a02681d
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-26 23:11:34 -0400

    Realizando la Migrations

--- ProyectoFinalJR/Migrations/20251127023339_AgregandoTipoProveedor.Designer.cs
+++ ProyectoFinalJR/Migrations/20251127023339_AgregandoTipoProveedor.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinalJR.Data;
 
@@ -11,9 +12,11 @@ using ProyectoFinalJR.Data;
 namespace ProyectoFinalJR.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251127023339_AgregandoTipoProveedor")]
+    partial class AgregandoTipoProveedor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProyectoFinalJR/Migrations/20251127023339_AgregandoTipoProveedor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalJR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoTipoProveedor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TiposProveedores",
+                columns: table => new
+                {
+                    TipoProveedorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposProveedores", x => x.TipoProveedorId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TiposProveedores");
+        }
+    }
+}
