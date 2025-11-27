24ed1ad4994323179816e042592256ecb5cca7fa
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2025-11-27 17:00:47 -0500

    Realizando migracion

---
+++ ProyectoFinalJR/Migrations/20251127213852_Iniciando.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalJR.Migrations
+{
+    /// <inheritdoc />
+    public partial class Iniciando : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Citas",
+                columns: table => new
+                {
+                    CitaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaCita = table.Column<DateOnly>(type: "date", nullable: false),
+                    HoraCita = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    UsuarioId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventoId = table.Column<int>(type: "int", nullable: false),
+                    ProveedorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citas", x => x.CitaId);
+                    table.ForeignKey(
+                        name: "FK_Citas_Eventos_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "Eventos",
+                        principalColumn: "EventoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_TiposProveedores_ProveedorId",
+                        column: x => x.ProveedorId,
+                        principalTable: "TiposProveedores",
+                        principalColumn: "TipoProveedorId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_EventoId",
+                table: "Citas",
+                column: "EventoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_ProveedorId",
+                table: "Citas",
+                column: "ProveedorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Citas");
+        }
+    }
+}
