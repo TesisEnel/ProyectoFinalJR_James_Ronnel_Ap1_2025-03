316538c6d4eb122101186280a901cb4bc25bcbfd
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-11-30 23:30:04 -0400

    refactor ♻️: cambios que no afectan la estrucura del codigo pero agrega la validacion del pago

---
+++ ProyectoFinalJR/Migrations/20251201031654_AddCamposTarjetaToPagos.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalJR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCamposTarjetaToPagos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CVV",
+                table: "PagosDetalle",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FechaVencimiento",
+                table: "PagosDetalle",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumeroTarjeta",
+                table: "PagosDetalle",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVV",
+                table: "PagosDetalle");
+
+            migrationBuilder.DropColumn(
+                name: "FechaVencimiento",
+                table: "PagosDetalle");
+
+            migrationBuilder.DropColumn(
+                name: "NumeroTarjeta",
+                table: "PagosDetalle");
+        }
+    }
+}
