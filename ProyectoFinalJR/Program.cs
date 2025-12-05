2c1b03a9704c038a0381cb88f23d62643c03c252
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-12-04 23:18:51 -0400

    refactor ♻️:  elimina lo del SeedData, y es pasado al ModelCreate

--- ProyectoFinalJR/Program.cs
+++ ProyectoFinalJR/Program.cs
@@ -61,11 +61,6 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSe
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    await SeedData.InitializeAsync(scope.ServiceProvider);
-}
-
 
 if (app.Environment.IsDevelopment())
 {
